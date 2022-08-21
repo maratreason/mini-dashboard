bf35e2b16145e039eca7f6e4c4303e78ea782489
maratreason <maratreason@mail.ru>
2022-08-21 14:09:22 +0300

    initial commit

---
+++ src/js/orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+    {
+        productName: "Ноутбук macintosh",
+        productNumber: "85631",
+        paymentStatus: "Не оплачено",
+        shipping: "В ожидании",
+    },
+    {
+        productName: "iPad",
+        productNumber: "35631",
+        paymentStatus: "Возмещено",
+        shipping: "Отклонено",
+    },
+    {
+        productName: "iPhone 11",
+        productNumber: "85631",
+        paymentStatus: "Не оплачено",
+        shipping: "В ожидании",
+    },
+    {
+        productName: "Samsung TV",
+        productNumber: "02831",
+        paymentStatus: "Не оплачено",
+        shipping: "В ожидании",
+    },
+    {
+        productName: "E-Book 360",
+        productNumber: "43854",
+        paymentStatus: "Оплачено",
+        shipping: "Доставлено",
+    },
+    {
+        productName: "iPhone 4",
+        productNumber: "98631",
+        paymentStatus: "Оплачено",
+        shipping: "Доставлено",
+    },
+    {
+        productName: "Apple Macintosh 2022",
+        productNumber: "81643",
+        paymentStatus: "Не оплачено",
+        shipping: "В ожидании",
+    },
+    {
+        productName: "Samsung v20",
+        productNumber: "11111",
+        paymentStatus: "Оплачено",
+        shipping: "Доставлено",
+    },
+];
